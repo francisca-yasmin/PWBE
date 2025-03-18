6afbb2e08263cdfc5e4e115c3fe37e828f4a9000
Francisca Yasmin Freitas de Almeida <47915176870@>
2025-03-18 10:12:44 -0300

    doc

--- revisao/API/api/api_app/urls.py
+++ revisao/API/api/api_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('carros/', views.read_carros),
     path('carros/buscar/<int:pk>', views.pegar_carro),
+    path('carros/criar/', views.create_carro),
+    path('carros/atualizar/<int:pk>', views.update_carro),
+    path('carros/apagar/<int:pk>', views.delete_carro)
 ]
--- revisao/API/api/api_app/views.py
+++ revisao/API/api/api_app/views.py
@@ -23,5 +23,35 @@ def pegar_carro(request, pk):
     serializer = CarroSerializer(carro)
     return Response(serializer.data)
 
+@api_view(['POST']) #processar
+def create_carro(request):
+    if request.method == 'POST':
+        serializer = CarroSerializer(data=request.data, many=isinstance(request.data, list)) #converter o JSON para carro
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['PUT'])
+def update_carro(request, pk):
+    try:
+        carro = Carro.objects.get(pk = pk)
+    except Carro.DoesNotExist:
+        return Response({'erro': ' Carro inexistente'}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = CarroSerializer(carro, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return  Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_carro(requeset, pk):
+    try:
+        carro = Carro.objects.get(pk = pk)
+    except Carro.DoesNotExist:
+        return Response({'erro': ' Carro inexistente'}, status=status.HTTP_404_NOT_FOUND)
+    
+    carro.delete()
+    return Response({'Mensagem': f'o seu {carro.nome} foi apagado'}, status=status.HTTP_200_OK)
     
