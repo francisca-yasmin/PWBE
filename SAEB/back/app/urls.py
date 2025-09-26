1e3a915e3af0bccea46a81c6f58a5c3a3d3aefdf
Yasmin Freitas <yasmimfreitaz5@gmail.com>
2025-09-26 07:44:19 -0300

    atualização nova

--- SAEB/back/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('usuario/', UsuarioListCreate.as_view()),
-    path('tarefa/', TarefaListCreate.as_view()),
-    path('tarefa/<int:pk>/', TarefaRetriveUpdateDestroy.as_view())
-]
--- SAEB/front/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export function Header(){
-    return(
-        <header className="header">
-            <h1>Gerenciamento de Tarefas</h1>
-        </header>
-    );
-}
